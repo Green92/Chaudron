0aa063de1a3ab52e92bcb8d4bfa4ebc1f1cbc96b
Lotharioaps <lothario.aps@gmail.com>
2016-11-25 17:18:53 +0100

    Started to integrate incomes.

--- model/requests.hpp
+++ model/requests.hpp
@@ -16,7 +16,7 @@ class Requests {
 			outcomes.push_back(Outcome(POINSON_SLIM, 50));
 			outcomes.push_back(Outcome(FIERCE_CREATURE, 100));
 
-			reqs[0] = Request("I need protection");
+			reqs[0] = Request("I need protection", outcomes);
 			outcomes.clear();
 
 			outcomes.push_back(Outcome(EYES, 10));
@@ -24,7 +24,71 @@ class Requests {
 			outcomes.push_back(Outcome(HALLUCINOGENIC_INFUSION, 10));
 			outcomes.push_back(Outcome(EYES_DROP, 100));
 
-			reqs[1] = Request("I want something for my eyes");
+			reqs[1] = Request("I want something for my eyes", outcomes);
+			outcomes.clear();
+
+			outcomes.push_back(Outcome(INSECTS, 10));
+			outcomes.push_back(Outcome(MUSHROOMS, 10));
+			outcomes.push_back(Outcome(PRANCING_EYE, 10));
+			outcomes.push_back(Outcome(BLINDING_POTION, 50));
+			outcomes.push_back(Outcome(POISON_PASTE, 100));
+			outcomes.push_back(Outcome(SLUMBER_POTION, 20));
+			outcomes.push_back(Outcome(POINSON_SLIM, 20));
+			outcomes.push_back(Outcome(FIERCE_CREATURE, 50));
+			outcomes.push_back(Outcome(BLINDING_TOBACCO, 50));
+			outcomes.push_back(Outcome(LAST_BREAKFAST, 10));
+			outcomes.push_back(Outcome(CANCER_COOKIE, 20));
+
+			reqs[2] = Request("I want to harm him", outcomes);
+			outcomes.clear();
+
+			outcomes.push_back(Outcome(PRANCING_EYE, 10));
+			outcomes.push_back(Outcome(POISON_PASTE, 50));
+			outcomes.push_back(Outcome(POINSON_SLIM, 50));
+			outcomes.push_back(Outcome(FIERCE_CREATURE, 50));
+			outcomes.push_back(Outcome(LAST_BREAKFAST, 10));
+			outcomes.push_back(Outcome(CANCER_COOKIE, 20));
+
+			reqs[3] = Request("I want to kill him", outcomes);
+			outcomes.clear();
+
+			outcomes.push_back(Outcome(PRANCING_EYE, 50));
+			outcomes.push_back(Outcome(INSECTS, 10));
+			outcomes.push_back(Outcome(POINSON_SLIM, 20));
+			outcomes.push_back(Outcome(FIERCE_CREATURE, 10));
+			outcomes.push_back(Outcome(OCULAR_SHEEP, 100));
+
+			reqs[4] = Request("I need a new pet", outcomes);
+			outcomes.clear();
+
+			outcomes.push_back(Outcome(INSECTS, 10));
+			outcomes.push_back(Outcome(MUSHROOMS, 10));
+			outcomes.push_back(Outcome(PRANCING_EYE, 10));
+			outcomes.push_back(Outcome(BLINDING_POTION, 50));
+			outcomes.push_back(Outcome(POISON_PASTE, 100));
+			outcomes.push_back(Outcome(SLUMBER_POTION, 20));
+			outcomes.push_back(Outcome(POINSON_SLIM, 20));
+			outcomes.push_back(Outcome(FIERCE_CREATURE, 50));
+			outcomes.push_back(Outcome(BLINDING_TOBACCO, 50));
+			outcomes.push_back(Outcome(LAST_BREAKFAST, 10));
+			outcomes.push_back(Outcome(CANCER_COOKIE, 20));
+
+			reqs[5] = Request("I'm hungry", outcomes);
+			outcomes.clear();
+
+			outcomes.push_back(Outcome(INSECTS, 10));
+			outcomes.push_back(Outcome(MUSHROOMS, 10));
+			outcomes.push_back(Outcome(PRANCING_EYE, 10));
+			outcomes.push_back(Outcome(BLINDING_POTION, 50));
+			outcomes.push_back(Outcome(POISON_PASTE, 100));
+			outcomes.push_back(Outcome(SLUMBER_POTION, 20));
+			outcomes.push_back(Outcome(POINSON_SLIM, 20));
+			outcomes.push_back(Outcome(FIERCE_CREATURE, 50));
+			outcomes.push_back(Outcome(BLINDING_TOBACCO, 50));
+			outcomes.push_back(Outcome(LAST_BREAKFAST, 10));
+			outcomes.push_back(Outcome(CANCER_COOKIE, 20));
+
+			reqs[6] = Request("I want to see beyond", outcomes);
 			outcomes.clear();
 		}
 
